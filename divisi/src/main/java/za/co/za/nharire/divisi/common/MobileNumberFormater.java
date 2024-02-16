9e635d61cb8f15e48bd04753e2e4c71df13ae016
kodebossman <kuumuparuri@gmail.com>
2024-02-16 15:02:17 +0200

    DELETED UNWANTED CODE

--- divisi/src/main/java/za/co/za/nharire/divisi/common/MobileNumberFormater.java
+++
@@ -1,9 +0,0 @@
-package za.co.nharire.divisi.common;;
-
-public interface MobileNumberFormater {
-  String formatMsisdnMinimum(String msisdn);
-
-  boolean isValidMsisdn(String msisdn);
-
-  String formatMobileNumberInternational(String msisdn);
-}
